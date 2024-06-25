9d1952670f8c12f7893d26da2e11d59701e316aa
Harsh Gupta <harsh@felvin.com>
2024-06-25 10:40:32 +0530

    bump version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="gpt-repository-loader",
-    version="0.9.5",
+    version="0.9.6",
     author="Felvin",
     author_email="team@felvin.com",
     description="A utility to convert a Git repository into a text representation.",
